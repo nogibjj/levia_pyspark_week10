d221a6aa48b257a863ce288d7feed2bb50aa00fd
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-12 22:10:29 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,18 @@
-# test_main.py
-import main  # Assuming your main script is named main.py
+from pyspark.sql import SparkSession
+from main import main 
 
-def test_example():
-    # Example test function
-    assert True, "This is just a basic assertion to ensure tests run"
+def test_main():
+    # Setup Spark session for testing
+    spark = SparkSession.builder.appName("TestBostonHousePriceAnalysis").getOrCreate()
 
-if __name__ == "__main__":
-    test_example()
+    # Run the main function
+    main()
+
+    # Read the output data and perform assertions
+    df = spark.read.csv("output/avg_price_by_rooms.csv", header=True, inferSchema=True)
+    assert df.count() > 0
+
+    df = spark.read.csv("output/houses_more_than_4_rooms.csv", header=True, inferSchema=True)
+    assert df.count() > 0
+
+    spark.stop()
