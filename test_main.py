94b2a2e78ba0f801d3c2f104dcc6fa7755d3b369
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-12 20:52:27 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,13 +1,5 @@
-from main import add, subtract
-
-
-def test_add():
-    assert add(1, 2) == 3
-
-def test_subtract():
-    assert subtract(1, 2) == -1
+import main
 
 if __name__ == "__main__":
-    test_add()
-    test_subtract()
+
     print("Success")
