036f3b9175601d39330d3ea99740ed3490ac6d3e
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-12 20:51:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-def add(a, b):
-    return a + b
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 
-def subtract(a, b):
-    return a - b
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
 
-if __name__ == "__main__":
-    print("Waiting for the result:")
+# Basic info
+df.info()
+
+# Descriptive statistics
+df.describe()
+
+# Check for missing values
+df.isnull().sum()
+
+# Pairplot to visualize relationships
+sns.pairplot(df, hue="target")
+plt.show()
+
+# Correlation heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.show()
