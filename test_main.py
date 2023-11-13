6a939b18ee42e3ee2be29a2b3ecd944ad59eeee8
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-12 21:26:01 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
+# test_main.py
+import main  # Assuming your main script is named main.py
 
+def test_example():
+    # Example test function
+    assert True, "This is just a basic assertion to ensure tests run"
 
 if __name__ == "__main__":
-
-    print("Success")
+    test_example()
