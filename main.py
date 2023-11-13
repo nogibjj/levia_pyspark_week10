cba9a2a1a5f36a8ac2d71a4bf9ada222430665ca
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-11-12 21:13:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_openml
 
-# Fetch the Iris dataset
-iris = fetch_openml(name='iris', version=1)
-data = iris.data
-feature_names = data.columns
-target = iris.target
-
-# Convert target to numeric if it's in string format
-target = pd.Categorical(target).codes
-
-# Create a DataFrame
-df = pd.DataFrame(data, columns=feature_names)
-df['target'] = target
+# Load the 'tips' dataset from Seaborn
+df = sns.load_dataset('tips')
 
 # Basic info
 df.info()
@@ -26,7 +15,7 @@ df.describe()
 df.isnull().sum()
 
 # Pairplot to visualize relationships
-sns.pairplot(df, hue="target")
+sns.pairplot(df, hue="time")  # 'time' is a column in the 'tips' dataset
 plt.show()
 
 # Correlation heatmap
